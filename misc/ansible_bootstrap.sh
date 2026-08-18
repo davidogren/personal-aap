241cba1225fd339ff6b8fe34a27c2eadcc769ca1
David Ogren <dogren@fastmail.fm>
2026-08-18 11:33:13 -0400

    Add git config and sudo
    No longer assuming running as root, add git config so modification we make to playbooks can be pushed back

--- misc/ansible_bootstrap.sh
+++ misc/ansible_bootstrap.sh
@@ -11,13 +11,14 @@
 # and paste these commands rather than run them as a full fledged script
 # 
 #Run as root
-if [ "$EUID" -ne 0 ]; then
-    echo "Error: This script must be run as root or with sudo." >&2
-    exit 1
-fi
-subscription-manager unregister
-subscription-manager clean
-subscription-manager register --user dogren_admin 
-dnf install -y ansible-core cifs-utils git
-mount -t cifs -o username=ogren,vers=2.0 //192.168.0.98/sync /mnt/sync
+sudo subscription-manager unregister
+sudo subscription-manager clean
+sudo subscription-manager register --user dogren_admin 
+sudo dnf install -y ansible-core cifs-utils git
 
+git config --global user.name "David Ogren"
+git config --global user.email "dogren@fastmail.fm"
+#Commenting until I can make this idempotent
+#git clone git@github.com:davidogren/personal-aap.git
+mount -t cifs -o username=ogren,vers=2.0 //192.168.0.98/sync /mnt/sync
+#Leaving one manual step here, making sure 
